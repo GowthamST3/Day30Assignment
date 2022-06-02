5c202cb6066ec3fcdd57883ce8843f6fe9787350
ST <gowtham.st@dxc.com>
2022-06-03 00:30:10 +0530

    Added - Code for to InvoiceGenerator_2

---
+++ InvoiceGenerator_2.java
@@ -0,0 +1,17 @@
+package com.Bridgelabz_Day30Assignment;
+
+public class InvoiceGenerator_2 {
+
+	public double CalculateFare(Ride[] rides) {
+		return 0;
+	}
+
+	public double CalculateFare1(Ride[] rides) {
+		return 0;
+	}
+
+	public double CalculateFare11(Ride[] rides) {
+		return 0;
+	}
+
+}
